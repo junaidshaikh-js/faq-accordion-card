6ba4447e3b03e6f62799192cc62341537a6cb5ce
Junaid <sj.shaikhjunaid@gmail.com>
2021-01-27 11:25:07 +0530

    Initial Commit

---
+++ scripts/index.js
@@ -0,0 +1,19 @@
+let question = document.getElementsByClassName('question');
+let answer = document.getElementsByClassName('answer');
+
+
+for (let index = 0; index < question.length; index++) {
+    question[index].addEventListener('click', drop);
+
+    function drop() {
+        question[index].classList.toggle('active');
+
+        if (answer[index].style.display != 'block') {
+            answer[index].style.display = 'block';
+        }
+        else {
+            answer[index].style.display = 'none';
+        }
+    }
+}
+
